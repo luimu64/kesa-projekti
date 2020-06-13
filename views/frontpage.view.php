a5aaf13acc84e05a260a8fc5a5f804e49f45edd8
luimu <luimu@localhost.localdomain>
2020-06-13 15:28:31 +0300

    header ja footer

--- views/frontpage.view.php
+++ views/frontpage.view.php
@@ -1,10 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-</body>
-</html>
+<?php
+//if(isLoggedIn()){
+//   require "public/partials/loggedhead.php";
+// } else {
+require "public/partials/head.php";
+//}
+?>
