dc787346dec1a4112883e5a1995b5bbe29432fc7
luimu <luimu@localhost.localdomain>
2020-06-13 13:32:19 +0300

    Tyhjät pohja tiedostoja tehty.

---
+++ controllers/usercontroller.php
@@ -0,0 +1,5 @@
+<?php
+
+function indexController() {
+    require "views/frontpage.view.php";
+}
---
+++ public/index.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+set_include_path(dirname(__FILE__) . '/../');
+
+require_once 'router/Request.php';
+require_once 'router/Router.php';
+// require_once 'libraries/helpers.php';
+require_once 'controllers/usercontroller.php';
+require_once 'controllers/admincontroller.php';
+
+$router = new Router(new Request);
+
+$router->get('/', function($request) {
+  indexController();
+});
+// $router->get('/products', function($request) {
+//   productsController();
+// });
+// $router->get('/register', function($request) {
+//   registeringController();
+// });
+// $router->post('/register', function($request) {
+//   registeringController();
+// });
+
+// $router->get('/login', function($request) {
+//   loginController();
+// });
+
+// $router->post('/login', function($request) {
+//   loginController();
+// });
+
+// $router->get('/logout', function($request) {
+//   logoutController();
+// });
+
+// $router->get('/reserve', function($request) {
+//   if(isLoggedIn()){
+//     reservingController();
+//   } else {
+//     loginController();
+//   }
+// });
+// $router->post('/reserve', function($request) {
+//     reservingController();
+// });
+// $router->get('/add_product', function($request) {
+//   if(isLoggedIn()){
+//     addproductController();
+//   } else {
+//     loginController();
+//   }
+// });
+// $router->post('/add_product', function($request) {
+//   if(isLoggedIn()){
+//     addproductController();
+//   } else {
+//     loginController();
+//   }
+// });
+
+// $router->get('/gdpr', function($request) {
+//   gdprController();
+//});
+?>
